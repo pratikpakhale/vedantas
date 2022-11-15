7d67bfe4240529340ffde62dd5af9c89fb8c7323
Ubuntu <ubuntu@ip-172-31-7-196.ap-northeast-1.compute.internal>
2022-11-15 04:42:18 +0000

    files

---
+++ main.py
@@ -0,0 +1,64 @@
+import bs4 as bs
+import requests
+import os
+import re
+
+URL = 'https://www.sacred-texts.com/hin/sbe34/index.htm'
+BASE_URL = 'https://www.sacred-texts.com/hin/sbe34/'
+
+content = requests.get(URL).content
+soup = bs.BeautifulSoup(content, 'html.parser')
+
+
+def filter(text):
+    return re.sub(r'[^\x00-\x7F]+', ' ', text)
+
+
+hr = soup.findAll('hr')[3]
+print(hr)
+FOLDER = ''
+next = hr.find_next_sibling()
+while next:
+    try:
+        if next.name == 'hr':
+            break
+        if next.name == 'h3':
+            FOLDER = next.text
+        if next.name == 'a' and len(FOLDER) > 0:
+            print(FOLDER)
+            try:
+                link = next['href']
+            except:
+                next = next.find_next_sibling()
+                continue
+            print(next['href'])
+            try:
+                c = requests.get(BASE_URL + next['href']).content
+            except:
+                print('Error')
+                next = next.find_next_sibling()
+                continue
+            # print(c)
+            s = bs.BeautifulSoup(c, 'html.parser')
+
+            text = ''
+            h = s.find('hr')
+            next_sibling = h.find_next_sibling()
+
+            while next_sibling:
+                if next_sibling.name == 'hr':
+                    break
+                current_text = next_sibling.text.strip()
+                if len(current_text) > 0:
+                    text += filter(current_text).strip() + '\n'
+                next_sibling = next_sibling.find_next_sibling()
+
+            titleDirExists = os.path.isdir(FOLDER)
+            if not titleDirExists:
+                os.makedirs(FOLDER)
+            with open(os.path.join(FOLDER, next.text + '.txt'), 'w') as f:
+                f.write(text)
+    except:
+        pass
+    next = next.find_next_sibling()
+
