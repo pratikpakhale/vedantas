c9a76b0198481503f56b70528001fb4efd5f7099
Ubuntu <ubuntu@ip-172-31-7-196.ap-northeast-1.compute.internal>
2022-11-15 05:23:03 +0000

    files

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ while next:
             s = bs.BeautifulSoup(c, 'html.parser')
 
             text = ''
-            h = s.find('hr')
+            h = s.findAll('hr')[2]
             next_sibling = h.find_next_sibling()
 
             while next_sibling:
